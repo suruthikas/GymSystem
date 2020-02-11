0a7721d6ab58b672a59f7f642bd15602f1a36990
Mohammed Choudhury <P2500583@my365.dmu.ac.uk>
2020-02-11 12:43:55 +0000

    Adding testing and class for customer

--- GymTesting/tstcustomer.cs
+++ GymTesting/tstcustomer.cs
@@ -14,5 +14,68 @@ namespace GymTesting
             //test to see that it exits
             Assert.IsNotNull(AnCustomer);
         }
+
+        [TestMethod]
+        public void customerID()
+        {
+            //create an instance of the class we want to create
+            CLSCustomer AnCustomer = new CLSCustomer();
+            Int32 TestData = 1;
+            //assign the data to the table
+            AnCustomer.customerID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.customerID, TestData);
+        }
+
+        [TestMethod]
+        public void customerUserName()
+        {
+            //create an instance of the class we want to create
+            CLSCustomer AnCustomer = new CLSCustomer();
+            //create some test data to assign to the table 
+            string TestData = "Mohammed99";
+            //assign the data to the table
+            AnCustomer.customerUserName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.customerUserName, TestData);
+        }
+
+        [TestMethod]
+        public void customerFirstName()
+        {
+            //create an instance of the class we want to create
+            CLSCustomer AnCustomer = new CLSCustomer();
+            //create some test data to assign to the table 
+            string TestData = "Mohammed";
+            //assign the data to the table
+            AnCustomer.customerFirstName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.customerFirstName, TestData);
+        }
+
+        [TestMethod]
+        public void customerlastName()
+        {
+            //create an instance of the class we want to create
+            CLSCustomer AnCustomer = new CLSCustomer();
+            //create some test data to assign to the table 
+            string TestData = "Choudhury";
+            //assign the data to the table
+            AnCustomer.customerlastName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.customerlastName, TestData);
+        }
+
+        [TestMethod]
+        public void customerActive()
+        {
+            //create an instance of the class we want to create
+            CLSCustomer AnCustomer = new CLSCustomer();
+            //create some test data to assign to the table 
+            Boolean TestData = true;
+            AnCustomer.customerActive = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnCustomer.customerActive, TestData);
+        }
     }
 }
