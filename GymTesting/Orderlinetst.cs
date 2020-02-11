5a84fb9c3c2e386fa70943534e8f97d95132d1b8
Mohammed Choudhury <P2500583@my365.dmu.ac.uk>
2020-02-11 11:15:38 +0000

    new master

---
+++ GymTesting/Orderlinetst.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GymClasses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GymTesting
+{
+    [TestClass]
+    public class Orderlinetst
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrderLine anOrderLine = new clsOrderLine();
+            Assert.IsNotNull(anOrderLine);
+
+
+
+
+        }
+    }
+}
