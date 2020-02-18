99ae06c089761694b75ccd90f6bf736f8610907c
Suruthika Sakthibalan <P2442165@my365.dmu.ac.uk>
2020-02-18 12:21:48 +0000

    Created Design View for OrderLine

--- GymFrontOffice/OrderLineViewer.aspx.cs
+++ GymFrontOffice/OrderLineViewer.aspx.cs
@@ -6,25 +6,12 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using GymClasses;
 
-public partial class AnOrderLine : System.Web.UI.Page
+public partial class OrderLineViewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
         clsOrderLine anOrderLine = new clsOrderLine();
         anOrderLine = (clsOrderLine)Session["anOrderLine"];
         Response.Write(anOrderLine.OrderNo);
-    }
-
-    protected void TextBox2_TextChanged(object sender, EventArgs e)
-    {
-
-    }
-
-    protected void BtnOK_Click(object sender, EventArgs e)
-    {
-        clsOrderLine anOrderLine = new clsOrderLine();
-        
-        Session["anOrderLine"] = anOrderLine;
-        Response.Redirect("OrderLineViewer.aspx");
     }
 }
