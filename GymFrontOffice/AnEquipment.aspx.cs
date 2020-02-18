5fa88bc9887b1a0d50239ff4da3155eef14165b2
Misha Chaudhry <P2508253@my365.dmu.ac.uk>
2020-02-18 08:38:49 +0000

    Added design view

---
+++ GymFrontOffice/AnEquipment.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary;
+
+public partial class AnEquipment : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        clsEquipment AnEquipment = new clsEquipment();
+        AnEquipment = (clsEquipment)Session["AnEquipment"];
+        Response.Write(AnEquipment.EquipmentNo);
+    }
+}
+
